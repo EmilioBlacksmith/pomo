1e4a66b97bdc68c27ac21b975c95c9ff13db8ab3
emilioblacksmith <emilioblacksmithlush@gmail.com>
2026-02-12 22:25:07 -0600

    feat: implement timer and circle in the app

---
+++ src/lib/components/circle.svelte
@@ -0,0 +1,22 @@
+<script>
+	export let progress = 100;
+
+	$: offset = 100 - progress;
+</script>
+
+<svg width="300" height="300" viewBox="0 0 130 130" style="transform: rotate(-90deg);">
+	<circle cx="65" cy="65" r="61.5" fill="none" stroke="#008537" stroke-width="10" />
+
+	<circle
+		cx="65"
+		cy="65"
+		r="61.5"
+		fill="none"
+		stroke="lightgreen"
+		stroke-width="10"
+		pathLength="100"
+		stroke-dasharray="100"
+		stroke-dashoffset={offset}
+		style="transition: stroke-dashoffset 1s linear;"
+	/>
+</svg>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Logo from '$lib/components/logo.svelte';
+	import Circle from '$lib/components/circle.svelte';
+</script>
+
+<main
+	class="min- flex min-h-screen w-screen flex-1 flex-col items-center justify-center bg-emerald-950 align-middle text-lime-200"
+>
+	<Circle />
+	<Logo />
+</main>
