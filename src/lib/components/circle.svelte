9c04aa062945237192a8ad3ac6de0e99c2f85908
emilioblacksmith <emilioblacksmithlush@gmail.com>
2026-02-12 22:31:07 -0600

    feat: small fixes in color and design

--- src/lib/components/circle.svelte
+++ src/lib/components/circle.svelte
@@ -4,16 +4,14 @@
 	$: offset = 100 - progress;
 </script>
 
-<svg width="300" height="300" viewBox="0 0 130 130" style="transform: rotate(-90deg);">
-	<circle cx="65" cy="65" r="61.5" fill="none" stroke="#008537" stroke-width="10" />
-
+<svg width="400" height="400" viewBox="0 0 130 130" style="transform: rotate(-90deg);">
 	<circle
 		cx="65"
 		cy="65"
 		r="61.5"
 		fill="none"
-		stroke="lightgreen"
-		stroke-width="10"
+		stroke="#008537"
+		stroke-width="6"
 		pathLength="100"
 		stroke-dasharray="100"
 		stroke-dashoffset={offset}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,6 @@
 <main
 	class="min- flex min-h-screen w-screen flex-1 flex-col items-center justify-center bg-emerald-950 align-middle text-lime-200"
 >
-	<Circle />
+	<Circle progress={10} />
 	<Logo />
 </main>
